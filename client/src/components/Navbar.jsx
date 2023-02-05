aee2e95e99be58664050282ff0b93c108faf26e1
Tim La <tlamarketing111@gmail.com>
2023-02-05 02:45:45 -0500

    building layout for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,11 @@ import logo from "../../images/logo.png";
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+        </nav>
     )
 }
 

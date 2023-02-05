5654d9b4ca9815809007e3d425fa07090a87eec4
Tim La <tlamarketing111@gmail.com>
2023-02-05 02:46:21 -0500

    created reusable navbaritem

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,6 +4,11 @@ import { AiOutlineClose } from "react-icons/ai";
 
 import logo from "../../images/logo.png";
 
+const NavBarItem = ({ title, classprops }) => (
+    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+);
+
+
 const Navbar = () => {
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">

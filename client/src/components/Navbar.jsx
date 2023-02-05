1db868f0ff0232dc8718df76842c4e895239d51f
Tim La <tlamarketing111@gmail.com>
2023-02-05 02:42:41 -0500

    import imports for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from "react";
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "../../images/logo.png";
 
 const Navbar = () => {
     return (

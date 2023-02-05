1ca601f7bb47574f188260534acd6e8d27d97dc8
Tim La <tlamarketing111@gmail.com>
2023-02-05 02:46:57 -0500

    added items for navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -15,6 +15,14 @@ const Navbar = () => {
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                    <NavBarItem key={item + index} title={item} />
+                ))}
+                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+                    Login
+                </li>
+            </ul>
         </nav>
     )
 }

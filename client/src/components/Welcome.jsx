39ff2b0127d0ce0cbd339af5d4a6baf4896a82a6
Tim La <tlamarketing111@gmail.com>
2023-02-05 02:52:24 -0500

    imported imports for Welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
+import { Loader } from ".";
 
 const Welcome = () => {
     return (

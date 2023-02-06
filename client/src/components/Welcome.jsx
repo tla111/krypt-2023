c7ce854f505cbac867b623f8b02ce30c4f566c77
Tim La <tlamarketing111@gmail.com>
2023-02-06 08:25:29 -0500

    implemented connect wallet button

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -16,6 +16,16 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
+                    <button
+                        type="button"
+                        // onClick={connectWallet}
+                        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+                    >
+
+                        <p className="text-white text-base font-semibold">
+                            Connect Wallet
+                        </p>
+                    </button>
                 </div>
             </div>
         </div>

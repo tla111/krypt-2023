0a151de68002c4e4e85c0d693022facfde0af2e5
Tim La <tlamarketing111@gmail.com>
2023-02-06 08:23:15 -0500

    building layout for welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,13 +3,17 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
-import { TransactionContext } from "../context/TransactionContext";
-import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const Welcome = () => {
     return (
-        <div>Welcome</div>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+
+                </div>
+            </div>
+        </div>
     )
 }
 

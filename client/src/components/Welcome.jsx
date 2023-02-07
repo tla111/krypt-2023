2d8ec58abba99d373fea800a0d2d3cc3ca43db01
Tim La <tlamarketing111@gmail.com>
2023-02-07 08:05:33 -0500

    created logic for input

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,6 +7,17 @@ import { Loader } from ".";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+        placeholder={placeholder}
+        type={type}
+        step="0.0001"
+        value={value}
+        onChange={(e) => handleChange(e, name)}
+        className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+);
+
 const Welcome = () => {
     return (
         <div className="flex w-full justify-center items-center">
@@ -65,9 +76,7 @@ const Welcome = () => {
                                 </div>
                             </div>
                         </div>
-
                     </div>
-
                 </div>
             </div>
         </div>
